0b41f49cde3303655286ee06e5afa36a910b0978
slavo3dev <slavo@slavo3.com>
2021-05-05 14:19:18 -0400

    importing herocomponents

--- componets /Hero/index..js
+++ componets /Hero/index..js
@@ -1,6 +1,23 @@
+import Image from 'next/image'
 import "./hero.module.css"
 
 export function Hero() {
     
+
+    return (
+        <section className={classes.hero}>
+            <div className={classes.image}>
+                <Image
+                    src="/images/site/profile.jpeg"
+                    alt="Profile Image"
+                    width={300}
+                    height={300} />
+                <h1>Wellocome to foo(nomad)</h1>
+                <p>
+                    I blog about digital nomad life style!! 
+                </p>
+          </div>
+        </section>
+    )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import {Fragment} from "react";
-
+import { Hero, FeaturedPosts } from "../componets"
 function HomePage() {
     
     return (
