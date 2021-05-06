2856b4a0ca3f32ca7001e74b9e2061ceaaa74f0d
slavo3dev <slavo@slavo3.com>
2021-05-06 17:33:40 -0400

    adding all post page

--- componets/Posts/AllPosts/index.js
+++ componets/Posts/AllPosts/index.js
@@ -1,3 +1,13 @@
+import { PostsGrid } from "../Postsgrid";
 import classes from "./allposts.module.css";
+import PropTypes from "prop-types";
 
-export function AllPosts() {}
+export function AllPosts() {
+  const dummyData = [];
+  return (
+    <section className={classes.posts}>
+      <h1>All Posts</h1>
+      <PostsGrid posts={dummyData} />
+    </section>
+  );
+}
