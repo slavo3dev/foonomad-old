5cb7f7e153a988531d067602d5b53972a23a5e53
slavo3dev <slavo@slavo3.com>
2021-05-13 17:34:37 -0400

    update

--- next.config.js
+++ next.config.js
@@ -1,22 +1,7 @@
-const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+const nextEnv = require("next-env");
+const dotenvLoad = require("dotenv-load");
 
-module.exports = (phase) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      env: {
-        mongodb_username: "slavo3",
-        mongodb_password: "Smimicom24011bg",
-        mongodb_contact_cluster: "mimicom24",
-        mongodb_database: "foonomaddb-dev",
-      },
-    };
-  }
-  return {
-    env: {
-      mongodb_username: "slavo3",
-      mongodb_password: "Smimicom24011bg",
-      mongodb_contact_cluster: "mimicom24",
-      mongodb_database: "foonomaddb",
-    },
-  };
-};
+dotenvLoad();
+
+const withNextEnv = nextEnv();
+module.exports = withNextEnv();
