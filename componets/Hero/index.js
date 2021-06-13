a43fe7ba3d577eb94f7c509ee30eaae57a29c12c
slavo3dev <slavo@slavo3.com>
2021-06-13 17:02:30 -0400

    chatbot

--- componets/Hero/index.js
+++ componets/Hero/index.js
@@ -1,22 +1,19 @@
-import Image from 'next/image'
-import classes from  "./hero.module.css"
+import Image from "next/image";
+import classes from "./hero.module.css";
 
 export function Hero() {
-
-    return (
-        <section className={classes.hero} >
-            <div className={classes.image}>
-                <Image
-                    src="/images/site/profile.jpeg"
-                    alt="Profile Image"
-                    width={300}
-                    height={300} />
-                </div>
-                <h1>Welcome to foo(nomad)</h1>
-                <p>
-                    I blog about digital nomad life style!! 
-                </p>
-        </section>
-    )
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/profile.jpeg"
+          alt="Profile Image"
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Welcome to foo(nomad)</h1>
+      <p>Science & Fun Facts with Chatbot Slavo</p>
+    </section>
+  );
 }
-
